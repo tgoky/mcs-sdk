a66faa56c23d616b3ddda9875a9022d3a6a6abe6
tgoky <tgokyicloud6@gmail.com>
2026-07-05 17:11:03 +0000

    first run executed successful

---
+++ drizzle/migrations/0003_dusty_pyro.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "engagements" ADD COLUMN "raw_voice_corpus" text;--> statement-breakpoint
+ALTER TABLE "engagements" ADD COLUMN "paste_ready_html" text;--> statement-breakpoint
+ALTER TABLE "engagements" ADD COLUMN "paste_ready_instructions" text;
